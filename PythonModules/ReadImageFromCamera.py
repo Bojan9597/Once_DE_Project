3494b013d73c00f5450b7598f8f44d36b1effd30
Bojan9597 <bojanpejic97@gmail.com>
2023-04-28 18:53:44 +0200

    ODP-1: Video Stream with Clickable Image Coordinates

---
+++ PythonModules/ReadImageFromCamera.py
@@ -0,0 +1,37 @@
+import cv2
+
+# Open a video capture object
+cap = cv2.VideoCapture(0)
+
+# Create a window to display the video stream
+cv2.namedWindow('Video Stream')
+
+# Define a mouse callback function
+def mouse_callback(event, x, y, flags, param):
+    # If left button is clicked, save the coordinates to file
+    if event == cv2.EVENT_LBUTTONDOWN:
+        with open('coordinates.txt', 'a') as f:
+            f.write('{}, {}\n'.format(x, y))
+
+# Set the mouse callback function for the window
+cv2.setMouseCallback('Video Stream', mouse_callback)
+
+# Loop through the video frames
+while True:
+    # Read a frame from the video stream
+    ret, frame = cap.read()
+
+    # If the frame was not read successfully, break out of the loop
+    if not ret:
+        break
+
+    # Display the frame in the window
+    cv2.imshow('Video Stream', frame)
+
+    # Exit the loop if the 'q' key is pressed
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# Release the video capture object and close the window
+cap.release()
+cv2.destroyAllWindows()
